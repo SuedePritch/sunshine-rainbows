c6905aa60d128d0280596248e1f41c882a203fd3
Your Name <james.edward.pritchard@gmail.com>
2022-05-28 10:52:37 -0400

    setup api call based on city name

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var apiKey = "0e55a5a5786ba0f434d9a700c4847843"
+var cityName = "Toronto"
+var cityLongitude;
+var cityLatitude;
+// http://api.openweathermap.org/geo/1.0/direct?q=Toronto&appid=0e55a5a5786ba0f434d9a700c4847843
+
+//find lat and long from city name
+//https://openweathermap.org/api/geocoding-api
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        cityLongitude = data[0].lon
+        cityLatitude = data[0].lat
+    }).then(function (){
+        //generic api call for weather
+        //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+        //testing with toronto
+        //https://api.openweathermap.org/data/2.5/onecall?lat=43.6534817&lon=-79.3839347&appid=0e55a5a5786ba0f434d9a700c4847843
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&appid=${apiKey}`)
+        .then(function (response) {
+            return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+            })
+    })
+
+
+
+
+
