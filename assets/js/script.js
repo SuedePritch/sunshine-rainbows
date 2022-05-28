59d3939c43fb0a7828548d84e1bd0c6a669cb50f
Your Name <james.edward.pritchard@gmail.com>
2022-05-28 15:55:27 -0400

    search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 var now = dayjs().format('dddd MMM D YYYY')
 var apiKey = "0e55a5a5786ba0f434d9a700c4847843"
-var cityName = "Atlanta"
 var cityLongitude;
 var cityLatitude;
 // http://api.openweathermap.org/geo/1.0/direct?q=Toronto&appid=0e55a5a5786ba0f434d9a700c4847843
+var searchFieldEl = document.getElementById('search')
+var searchSubmitEl = document.getElementById('searchSubmit')
+var searchText;
+var cityName;
+
 var currentCityNameEl = document.getElementById('current-cityname')
 var currentDateEl = document.getElementById('current-date')
 var currentTempEl = document.getElementById('current-temp')
@@ -11,15 +15,28 @@ var currentConditionEl = document.getElementById('current-conditions')
 var currentHumidityEl = document.getElementById('current-humidity')
 var currentWindspeedEl = document.getElementById('current-windspeed')
 var currentUVIndexEl = document.getElementById('current-uvindex')
-//find lat and long from city name
-//https://openweathermap.org/api/geocoding-api
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
+
+function loadCurrentWeather(){
+    var storedCityData = JSON.parse(localStorage.getItem(cityName))
+    currentCityNameEl.textContent = cityName
+    currentDateEl.textContent = now
+    currentTempEl.textContent = `Temp: ${Math.floor(storedCityData.current.temp - 273)} C`
+    currentConditionEl.textContent = storedCityData.current.weather[0].main
+    currentHumidityEl.textContent = `Humidity: ${storedCityData.current.humidity}`
+    currentWindspeedEl.textContent = `Wind Speed: ${storedCityData.current.wind_speed}`
+    currentUVIndexEl.textContent = `UV index:${storedCityData.current.uvi}`
+}        
+
+function fetchWeatherData(){
+    //find lat and long from city name
+    //https://openweathermap.org/api/geocoding-api
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`)
     .then(function (response) {
-    return response.json();
+        return response.json();
     })
     .then(function (data) {
-        cityLongitude = data[0].lon
         cityLatitude = data[0].lat
+        cityLongitude = data[0].lon
     }).then(function (){
         //generic api call for weather
         //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
@@ -31,16 +48,14 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey
             })
             .then(function (data) {
                 localStorage.setItem(cityName, JSON.stringify(data))
+                loadCurrentWeather();
             })
         })
-        
-// console.log(data.current.temp);
-var storedCityData = JSON.parse(localStorage.getItem(cityName))
-currentCityNameEl.textContent = cityName
-currentDateEl.textContent = now
-currentTempEl.textContent = `Temp: ${Math.floor(storedCityData.current.temp - 273)}`
-currentConditionEl.textContent = storedCityData.current.weather[0].main
-currentHumidityEl.textContent = `Humidity: ${storedCityData.current.humidity}`
-currentWindspeedEl.textContent = `Wind Speed: ${storedCityData.current.wind_speed}`
-currentUVIndexEl.textContent = `UV index:${storedCityData.current.uvi}`
 
+    }
+searchSubmitEl.addEventListener('click', function(){
+    searchText = searchFieldEl.value
+    cityName = searchText
+    console.log(cityName);
+    fetchWeatherData();
+})
