9dec65b92c209d0a19e1f87836d3ba5dc39f1c89
Your Name <james.edward.pritchard@gmail.com>
2022-05-29 13:10:39 -0400

    added five day forecast, styling changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,64 @@ var currentHumidityEl = document.getElementById('current-humidity')
 var currentWindspeedEl = document.getElementById('current-windspeed')
 var currentUVIndexEl = document.getElementById('current-uvindex')
 
+
 function loadCurrentWeather(){
     var storedCityData = JSON.parse(localStorage.getItem(cityName))
     currentCityNameEl.textContent = `${cityName.name}, ${cityName.state}, ${cityName.country} `
     currentDateEl.textContent = now
     currentTempEl.textContent = `Temp: ${Math.floor(storedCityData.current.temp - 273)} C`
     var icon = storedCityData.current.weather[0].icon
-    currentConditionEl.innerHTML = `<img src=https://openweathermap.org/img/wn/${icon}@4x.png>`
-    
+    var iconDescription = storedCityData.current.weather[0].description
+    currentConditionEl.innerHTML = 
+    `<figure>
+        <img src="https://openweathermap.org/img/wn/${icon}@4x.png">
+        <figcaption id="current-condition-description">${iconDescription}</figcaption>
+    </figure>`
     currentHumidityEl.textContent = `Humidity: ${storedCityData.current.humidity}`
     currentWindspeedEl.textContent = `Wind Speed: ${storedCityData.current.wind_speed}`
     currentUVIndexEl.textContent = `UV index:${storedCityData.current.uvi}`
 }        
+
+function loadFiveDayForcast(){
+    var storedCityData = JSON.parse(localStorage.getItem(cityName))
+    $('#five-day-forcast').text('')
+    for(var i = 0; i < 5; i++){
+        var iconSmall = `${storedCityData.daily[`${i}`].weather[0].icon}`
+        var date = dayjs().add(`${i}`,'day').format('dddd')
+        var dayContainerEl = $("<div>")
+        var dateDiv =      $("<div>");
+        var conditionDiv = $("<div>");
+        var conditionImg = $("<img>")
+        var tempDiv =      $("<div>");
+        var windDiv =      $("<div>");
+        var humidityDiv =  $("<div>");
+
+        dayContainerEl.addClass(`day`)
+        dayContainerEl.attr('id', `day-${i}`)
+        
+        dateDiv.attr('id',`date-${i}`)
+        conditionDiv.attr('id',`condition-${i}`)
+        tempDiv.attr('id',`temp-${i}`)
+        windDiv.attr('id',`wind-${i}`)
+        humidityDiv.attr('id',`humidity-${i}`)
+        dateDiv.text(date)
+        conditionImg.attr('src', `https://openweathermap.org/img/wn/${iconSmall}@2x.png`)
+        tempDiv.text(`${Math.floor(storedCityData.daily[`${i}`].temp.day) - 273}C` )
+        windDiv.text(`${storedCityData.daily[`${i}`].wind_speed}m/s`)
+        humidityDiv.text(`${storedCityData.daily[`${i}`].humidity}%`)
+        $('#five-day-forcast').append(dayContainerEl)
+        dayContainerEl.append(dateDiv, conditionDiv, tempDiv, windDiv, humidityDiv)
+        conditionDiv.append(conditionImg)
+        console.log(date);
+
+    }
+
+    }
+
+
+
+
+
 
 function fetchWeatherData(){
     //find lat and long from city name
@@ -41,8 +87,7 @@ function fetchWeatherData(){
             name:data[0].name,
             state:data[0].state,
             country:data[0].country
-        }
-        console.log(cityName);
+            }
         cityLatitude = data[0].lat
         cityLongitude = data[0].lon
     }).then(function (){
@@ -57,13 +102,20 @@ function fetchWeatherData(){
             .then(function (data) {
                 localStorage.setItem(cityName, JSON.stringify(data))
                 loadCurrentWeather()
+                loadFiveDayForcast()
                 
             })
-        })
-
     }
+    )}
+
+searchFieldEl.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        searchText = searchFieldEl.value
+        fetchWeatherData();
+    }
+})
 searchSubmitEl.addEventListener('click', function(){
     searchText = searchFieldEl.value
-    
     fetchWeatherData();
 })
+
